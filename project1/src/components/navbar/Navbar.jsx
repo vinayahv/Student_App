659dc4f5cd9db5b63cf6cb8e752824581e9101f8
Vinaya H v <vinayahegde303@gmail.com>
2023-02-23 12:06:56 +0530

    first commit

---
+++ project1/src/components/navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <h1>BRAND BOY</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ project1/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Hero from '../../components/hero/Hero'
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
