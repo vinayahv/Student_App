12d4fa4d38d86af387bc21e2dc588467567cab19
Vinaya H v <vinayahegde303@gmail.com>
2023-02-23 12:12:24 +0530

    second commit

--- project1/src/components/hero/Hero.jsx
+++ project1/src/components/hero/Hero.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae quam deserunt distinctio sunt sint sapiente 
+        impedit quibusdam qui laboriosam explicabo aliquid doloribus, tenetur dolores quod facere sit saepe porro magnam?</div>
     </div>
   )
 }
---
+++ project1/src/pages/contatc/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores dolore aliquam enim, maiores nisi eius, 
+        odit recusandae saepe aliquid id unde facere consectetur at. Maxime recusandae commodi corporis amet doloremque.
+      </div>
+    </div>
+  )
+}
+
+export default Contact
